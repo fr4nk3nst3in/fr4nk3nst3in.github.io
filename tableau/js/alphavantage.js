e7d4436aee6f9bb224f0add1abdc42c963a395f6
viktor frankenstein <fr4nk3nst3in@gmail.com>
2017-06-10 19:58:22 +0200

    save

---
+++ tableau/js/alphavantage.js
@@ -0,0 +1,59 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+    
+     	var cols = [
+     		date, open, high, low, close, volume
+     	
+        	{ id : "date", alias : "date", columnRole: "dimension",  dataType : tableau.dataTypeEnum.date },
+        	{ id : "open", alias : "open",  columnRole: "measure", dataType : tableau.dataTypeEnum.float  },
+        	{ id : "high", alias : "high",  columnRole: "measure", dataType : tableau.dataTypeEnum.float },
+        	{ id : "low", alias : "low", columnRole: "measure", dataType : tableau.dataTypeEnum.float },
+        	{ id : "close", alias : "close",columnRole: "measure", dataType : tableau.dataTypeEnum.float }
+        	{ id : "volume", alias : "volume",columnRole: "measure", dataType : tableau.dataTypeEnum.int }
+    	];
+
+    	var tableInfo = {
+        	id : "alphavantage",
+        	alias : "historical symbol data from alphavantage",
+        	columns : cols
+    	};
+
+    	schemaCallback([tableInfo]);
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+		$.getJSON("http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=RHT&apikey=KIO2ZPVEEHLOKGAZ", function(resp) {
+        		var feat = resp.features,
+            	tableData = [];
+
+        		// Iterate over the JSON object
+        		for (var i = 0, len = Time Series (Daily).length; i < len; i++) {
+            		tableData.push({
+                		"id": feat[i].id,
+                		"mag": feat[i].properties.mag,
+                		"title": feat[i].properties.title,
+                		"lon": feat[i].geometry.coordinates[0],
+                		"lat": feat[i].geometry.coordinates[1]
+            		});
+        		}
+
+        		table.appendRows(tableData);
+        		doneCallback();
+    	});
+    };
+
+    tableau.registerConnector(myConnector);
+        // Create event listeners for when the user submits the form
+    
+    $(document).ready(function() {
+        translateButton();
+        $("#submitButton").click(function() {
+            tableau.connectionName = "get data"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+    
+})();
